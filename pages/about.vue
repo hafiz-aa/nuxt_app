04fc16e3d59fc81b740efcb18cafd419783341f4
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-08 13:49:19 +0700

    initialize project

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that display corny dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About the App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+</style>
