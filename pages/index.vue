c7c386f91d0092f45f2bffe158a72b3b581c87bd
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-08 15:39:24 +0700

    Add AppHeader

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial />
+  <div>
+    <h2>Welcome to the funniest dad jokes site</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  head() {
+    return {
+      title: 'Welcome to Dad Jokes Site',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes',
+        },
+      ],
+    }
+  },
 }
 </script>
